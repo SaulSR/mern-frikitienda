56a6f46147070da6cef83f89d3e77beec12ff345
saul_salazar <gosth2605@hotmail.com>
2022-08-13 04:51:47 -0500

    imagenes,boton y responsividad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Friki-Tienda</a>
       </header>
-      <main>Lista De Productos</main>
+      <main>
+        <h1>Productos Destacados</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Añadir al Carrito</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
