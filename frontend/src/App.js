b28de4a4f9383bad0064a977dcdb15d1abd7b6d1
saul_salazar <gosth2605@hotmail.com>
2022-08-13 01:54:18 -0500

    Create-React-App-P5

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Friki-Tienda</a>
+      </header>
+      <main>Lista De Productos</main>
+    </div>
+  );
+}
+
+export default App;
