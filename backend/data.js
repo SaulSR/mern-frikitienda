aa7b512a5faacdc492a7c4777290e6436e6c0f80
saul_salazar <gosth2605@hotmail.com>
2022-08-15 14:29:28 -0500

    #

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: 'Spider-Man: Entre los Muertos',
+      slug: 'Comic-Spiderman',
+      category: 'Comics',
+      image: '/images/p1.jpg',
+      price: 760,
+      countInStock: 10,
+      brand: 'Marvel',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'La peor pesadilla de Spiderman se ha hecho realidad.',
+    },
+    //segundo producto
+    {
+      name: 'Lobezno: Origen',
+      slug: 'Comic-Lobezno',
+      category: 'Comics',
+      image: '/images/p2.jpg',
+      price: 700,
+      countInStock: 10,
+      brand: 'Marvel',
+      rating: 4.8,
+      numReviews: 18,
+      description: 'El comic que cuenta el origen de Wolverine',
+    },
+    //tercer producto
+    {
+      name: 'El Hombre sin Miedo',
+      slug: 'Comic-Hombre-Sin-Miedo',
+      category: 'Comics',
+      image: '/images/p3.jpg',
+      price: 560,
+      countInStock: 16,
+      brand: 'Marvel',
+      rating: 4.3,
+      numReviews: 24,
+      description:
+        'Comic Daredevil: El Hombre sin Miedo de Frank MiIler y John Romita Jr',
+    },
+    //cuarto producto
+    {
+      name: 'El Origen De Doctor strange ',
+      slug: 'Comic-Dr-Strange',
+      category: 'Comics',
+      image: '/images/p4.jpg',
+      price: 4720,
+      countInStock: 5,
+      brand: 'Marvel',
+      rating: 5,
+      numReviews: 28,
+      description:
+        'Roger Stern y Mike Mignola nos cuentan la alianza entre Doctor Extraño y Doctor Muerte en Triunfo y Tormento.',
+    },
+  ],
+};
+export default data;
